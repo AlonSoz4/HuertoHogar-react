63d6a8f05810b1521a553b251c28e60a4ea4761d
AlonSoz4 <alonso.soza@inacapmail.cl>
2026-07-10 22:04:22 -0400

    Comienzo nuevo HuertoHogar-react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-success">🌱 HuertoHogar React en camino</h1>
+      <p>Entorno configurado correctamente con Bootstrap.</p>
+    </div>
+  )
+}
+
+export default App
